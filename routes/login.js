e4800def4605c7cef5d32a5fadb7e3f9a91014d3
Dantescur <daniel@thedaniweb.eu.org>
2024-01-15 22:46:51 -0500

    M  routes/login.js M  routes/register.js

--- routes/login.js
+++ routes/login.js
@@ -5,42 +5,59 @@ const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const { User } = require("../db");
 const SECRET_KEY = process.env.SECRET_KEY;
-const { sendError } = require("../lib/errors");
 const logger = require("../lib/winston").logger;
+
+// Define a helper function that takes an async function and returns a middleware function
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 router.use((req, res, next) => {
   const schema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(12).required(),
+    username: Joi.string().alphanum().min(3).max(12).required().messages({
+      "string.base": "Username must be a string",
+      "string.alphanum": "Username must only contain alpha-numeric characters",
+      "string.min": "Username must be at least {#limit} characters",
+      "string.max": "Username cannot be longer than {#limit} characters",
+      "any.required": "Username is required",
+    }),
     password: Joi.string()
       .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
-      .required(),
+      .required()
+      .min(8)
+      .not(Joi.ref("username"))
+      .messages({
+        "string.pattern.base":
+          "Password must only contain alpha-numeric characters and be 3-30 characters long",
+        "any.required": "Password is required",
+        "string.min": "Password must be at least {#limit} characters",
+        "any.invalid": "Password cannot be the same as username",
+      }),
   });
-  const { error } = schema.validate(req.body);
+
+  const { error } = schema.validate(req.body, { abortEarly: false });
   if (error) {
-    return sendError(res, 400, "Invalid input. Please check your data", [
-      error.message,
-    ]);
+    const errorMessages = error.details.map((detail) => detail.message);
+    return res.status(400).json({
+      message: "Invalid input. Please check your data",
+      errors: errorMessages,
+    });
   }
   next();
 });
 
-router.post("/", async (req, res) => {
-  logger.info("Received request:", JSON.stringify(req.body, null, 2));
-  const { username, password } = req.body;
-  try {
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    logger.info("Received request:", JSON.stringify(req.body, null, 2));
+    const { username, password } = req.body;
     const user = await User.findOne({ where: { username } });
     handleLoginAttemp(user, password, res);
-  } catch (error) {
-    // Sending a successful login response with the user's username and token
-    logger.error("Error finding user:", error);
-    // Sending an error response
-    sendError(res, 500, "Internal server error");
-  }
-});
+  }),
+);
 
 const handleLoginAttemp = async (user, password, res) => {
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return sendError(res, 401, "Invalid credentials");
+    return res.status(401).json({ message: "Invalid credentials" });
   }
   const token = jwt.sign(
     {
--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,46 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 const { User } = require("../db");
 const SECRET_KEY = process.env.SECRET_KEY;
-const { sendError } = require("../lib/errors");
+const logger = require("../lib/winston").logger;
+
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
+router.use((req, res, next) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(12).required().messages({
+      "string.base": "Username must be a string",
+      "string.alphanum": "Username must only contain alpha-numeric characters",
+      "string.min": "Username must be at least {#limit} characters",
+      "string.max": "Username cannot be longer than {#limit} characters",
+      "any.required": "Username is required",
+    }),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required()
+      .min(8)
+      .not(Joi.ref("username"))
+      .messages({
+        "string.pattern.base":
+          "Password must only contain alpha-numeric characters and be 3-30 characters long",
+        "any.required": "Password is required",
+        "string.min": "Password must be at least {#limit} characters",
+        "any.invalid": "Password cannot be the same as username",
+      }),
+  });
+  const { error } = schema.validate(req.body, { abortEarly: false });
+  if (error) {
+    const errorMessages = error.details.map((detail) => detail.message);
+    return res.status(400).json({
+      message: "Invalid input. Please check your data",
+      errors: errorMessages,
+    });
+  }
+  next();
+});
 
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
@@ -16,20 +53,8 @@ router.post("/", async (req, res) => {
     const token = jwt.sign({ userId: user.id }, SECRET_KEY);
     res.status(200).json({ user: user.username, token });
   } catch (error) {
-    handleRegistrationError(error, res);
+    logger.error(error);
   }
 });
-
-const handleRegistrationError = (error, res) => {
-  if (error.name === "SequelizeUniqueConstraintError") {
-    return sendError(res, 400, "Username already exists");
-  } else if (error.name === "SequelizeValidationError") {
-    const errors = error.errors.map((e) => e.message);
-    return sendError(res, 400, "Invalid input. Please check your data", errors);
-  } else {
-    console.error("Error creating user: ", error);
-    sendError(res, 500, "Internal server error");
-  }
-};
 
 module.exports = router;
